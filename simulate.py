d78b5899e3625cb2044151c0b37d90add2a2de1b
kpomroy <kpomroy@uvm.edu>
2023-01-18 13:18:30 -0500

    create simulation environment

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
+from xml.etree.ElementInclude import include
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
 
 p.disconnect()
