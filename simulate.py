c88c89a630cb3ade4b5b4ef5b481bb50f57b1b44
kpomroy <kpomroy@uvm.edu>
2023-01-18 13:47:46 -0500

    create a box

--- simulate.py
+++ simulate.py
@@ -3,6 +3,10 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
+#read in the world described in box.sdf
+p.loadSDF("box.sdf")
+
+#loop to keep simulated environment open
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
