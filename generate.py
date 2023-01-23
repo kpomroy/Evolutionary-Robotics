a398b87f99be47a1cb95382ef4089e951eb134cf
kpomroy <kpomroy@uvm.edu>
2023-01-23 16:40:43 -0500

    Create a 3 link 2 joint robot

--- generate.py
+++ generate.py
@@ -4,42 +4,23 @@ length = 1
 width = 1
 height = 1
 
-x = 0
-y = 0
-z = 0.5
-
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[-2,2,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[-2,2,0.5] , size=[length,width,height])
     pyrosim.End()
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length,width,height])
 
-    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link0", pos=[1.5,0,1.5] , size=[length,width,height])
 
-    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [1,0,1])
 
-    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[-0.5,0,-0.5] , size=[length,width,height])
 
-    pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Link0_Link2" , parent= "Link0" , child = "Link2" , type = "revolute", position = [2,0,1])
 
-    pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0.5])
-
-    pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0] , size=[length,width,height])
-
-    pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0,1,0])
-
-    pyrosim.Send_Cube(name="Link4", pos=[0,0.5,0] , size=[length,width,height])
-
-    pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0,0.5,-0.5])
-
-    pyrosim.Send_Cube(name="Link5", pos=[0,0,-0.5] , size=[length,width,height])
-
-    pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0,0,-1])
-
-    pyrosim.Send_Cube(name="Link6", pos=[0,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Link2", pos=[0.5,0,-0.5] , size=[length,width,height])
 
     pyrosim.End()
 
