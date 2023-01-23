d2d7801aad207e67edfdeafb61d198c83a543350
kpomroy <kpomroy@uvm.edu>
2023-01-23 15:04:27 -0500

    Create initial joint between cubes

--- generate.py
+++ generate.py
@@ -16,6 +16,8 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="Leg", pos=[-0.5,0,0.5] , size=[length,width,height])
     pyrosim.End()
 
 Create_World()
