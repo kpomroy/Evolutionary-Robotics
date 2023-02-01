0a379cbb782c76545d4067bc29a63ae726d5d4cc
kpomroy <kpomroy@uvm.edu>
2023-02-01 16:11:45 -0500

    Add sensor to front leg and create plot

--- analyze.py
+++ analyze.py
@@ -3,8 +3,16 @@ import matplotlib.pyplot as matplotlib
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 
+frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+
 print(backLegSensorValues)
+print(frontLegSensorValues)
 
-matplotlib.plot(backLegSensorValues)
+matplotlib.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
+matplotlib.plot(frontLegSensorValues, label = "Front Leg")
 
+#add legend to plot
+matplotlib.legend()
+
+#show plot
 matplotlib.show()
--- simulate.py
+++ simulate.py
@@ -21,24 +21,32 @@ robotId = p.loadURDF("body.urdf")
 #prepare to simulate sensors
 pyrosim.Prepare_To_Simulate(robotId)
 
-#create vector of sensor values
-backLegSensorValues = np.zeros(100)
-
+#create vectors of sensor values
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
 #loop to keep simulated environment open
-for i in range(100):
+for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
 
     #create backleg touch sensor at each iteration of loop
     #save to backLegSensorValues array
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
+    #create frontleg touch sensor
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     
 print(backLegSensorValues)
+print(frontLegSensorValues)
 
-outfile = open("data/backLegSensorValues.npy", "wb")
-np.save(outfile, backLegSensorValues)
-outfile.close()
+backLegData= open("data/backLegSensorValues.npy", "wb")
+np.save(backLegData, backLegSensorValues)
+backLegData.close()
+
+frontLegData = open("data/frontLegSensorValues.npy", "wb")
+np.save(frontLegData, frontLegSensorValues)
+frontLegData.close()
 
 
 p.disconnect()
