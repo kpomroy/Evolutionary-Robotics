22295c11f1ecf6d2f70936c695c50e02ae69e1a6
kpomroy <kpomroy@uvm.edu>
2023-04-17 15:31:44 -0400

    Deleted unecessary comments

--- constants.py
+++ constants.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-"""
-backLegAmplitude = np.pi/4
-backLegFrequency = 5.5
-backLegPhaseOffset = .25
-frontLegAmplitude = np.pi/4
-frontLegFrequency = 5
-frontLegPhaseOffset = .25
-"""
 steps = 2000
 motorMaxForce = 50
 numberOfGenerations = 10
