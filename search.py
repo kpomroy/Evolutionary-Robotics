57f8096409d739b0ee0a8e752b494b2e28dbf592
kpomroy <kpomroy@uvm.edu>
2023-04-20 09:33:21 -0400

    remove fitness csv at start

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
+import os
 
+os.system("rm finalProject/fitness/hexapod1Fitness.csv")
 for i in range(1):
     phc = PARALLEL_HILL_CLIMBER()
     phc.Evolve()
