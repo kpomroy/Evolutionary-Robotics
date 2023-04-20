f9e06051deba484cb17263954257edfe00c63456
kpomroy <kpomroy@uvm.edu>
2023-04-20 14:05:51 -0400

    Saving matrix of fitness to txt

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -1,14 +1,19 @@
 import constants as c
 import copy
+import numpy as np
 import os
 from solution import SOLUTION
 
 class PARALLEL_HILL_CLIMBER:
 
-    def __init__(self):
+    def __init__(self, iteration):
         os.system("rm brain*.nndf")
         os.system("rm fitness*.txt")
+        #create matrix for fitnesses
+        self.fitnessMat = np.zeros(shape = (c.populationSize,c.numberOfGenerations))
         self.nextAvailableID = 0
+        self.generation = 0
+        self.iteration = iteration
         self.parents = {}
         for i in range (c.populationSize):
             self.parents[i] = SOLUTION(self.nextAvailableID)
@@ -45,20 +50,28 @@ class PARALLEL_HILL_CLIMBER:
 
     def Select(self):
         for i in self.parents:
+            self.fitnessMat[i][self.generation] = self.children[i].fitness
             if(self.parents[i].fitness > self.children[i].fitness):
+                #replace parent with child if more fit
                 self.parents[i] = self.children[i]
+                #delete brain file
                 deleteID = self.parents[i].myID
                 os.system("rm brain" + str(deleteID) + ".nndf")
             else:
+                #delete brain file
                 deleteID = self.children[i].myID
                 os.system("rm brain" + str(deleteID) + ".nndf")
+        self.generation+=1
+
+        if(self.generation == c.numberOfGenerations-1):
+            np.savetxt('finalProject/fitness/octopod2' + str(self.iteration) + '.txt' ,self.fitnessMat, fmt = '%.4f')
 
     def Print(self):
-        allFitnessFile = open("finalProject/fitness/octopod2Fitness.csv", "a")
+        allFitnessFile = open("finalProject/fitness/octopod2Fitness" + str(self.iteration) + ".csv", "a")
         if (os.stat("finalProject/fitness/octopod2Fitness.csv").st_size == 0):
             allFitnessFile.write("Family,Parent,Child\n")
         for i in self.parents:
-            allFitnessFile.write(str(i) + "," + str(self.parents[i].fitness) + "," + str(self.children[i].fitness) + "\n")
+            allFitnessFile.write(str(i) + "," + str(round(self.parents[i].fitness), 4) + "," + str(round(self.children[i].fitness, 4)) + "\n")
         allFitnessFile.close()
 
     def Show_Best(self, num):
@@ -70,6 +83,7 @@ class PARALLEL_HILL_CLIMBER:
                 bestFitness = self.parents[i].fitness
                 bestSolution = self.parents[i]
         bestSolution.Start_Simulation("GUI")
+
         print("")
         print("Best fitness:", bestFitness)
         print("Best solution ID: " + str(bestSolution.myID))
