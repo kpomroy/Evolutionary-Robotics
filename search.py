bf7b430115eb7d1d5b3a7c89ed81ed51380e916b
kpomroy <kpomroy@uvm.edu>
2023-04-20 08:39:34 -0400

    Saving brains to folder

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
+import time
 
-phc = PARALLEL_HILL_CLIMBER()
-phc.Evolve()
-phc.Show_Best()
+for i in range(5):
+    phc = PARALLEL_HILL_CLIMBER()
+    phc.Evolve()
+    phc.Show_Best(i)
