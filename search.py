2774b1d303e771b43c8c1b7a1467bd5b0ba28614
kpomroy <kpomroy@uvm.edu>
2023-04-24 13:37:20 -0400

    Reduced iterations to 1

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import os
 
 os.system("rm finalProject/fitness/octopod1Fitness.csv")
-for i in range(2):
+for i in range(1):
     phc = PARALLEL_HILL_CLIMBER(i)
     phc.Evolve()
     phc.Show_Best(i)
