45d7e232323fcdb8fdeb9af42aa4e488657cb297
kpomroy <kpomroy@uvm.edu>
2023-05-07 16:47:08 -0400

    Increased runs to 10

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import os
 
 os.system("rm finalProject/fitness/hexapod1Fitness.csv")
-for i in range(1):
+for i in range(10):
     phc = PARALLEL_HILL_CLIMBER(i)
     phc.Evolve()
     phc.Show_Best(i)
